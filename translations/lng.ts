a32e1be1258aabed4f95d03f5bceafeaa6d0fa47
SunRain <41245110@qq.com>
2015-06-07 15:00:41 +0800

    更新翻译

--- translations/lng.ts
+++ translations/lng.ts
@@ -2,172 +2,285 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
-    <name>main</name>
+    <name>OverrideController</name>
     <message>
-        <location filename="main.qml" line="14"/>
-        <source>Douban.FM</source>
+        <location filename="OverrideController.qml" line="229"/>
+        <location filename="OverrideController.qml" line="245"/>
+        <source>Not supported</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="18"/>
-        <location filename="main.qml" line="19"/>
-        <source>Like</source>
+        <location filename="OverrideController.qml" line="239"/>
+        <source>UnKnow</source>
         <translation type="unfinished"></translation>
     </message>
+</context>
+<context>
+    <name>Settings</name>
     <message>
-        <location filename="main.qml" line="18"/>
-        <location filename="main.qml" line="19"/>
-        <source>UnLike</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="main.qml" line="28"/>
-        <location filename="main.qml" line="29"/>
-        <source>Ban</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="main.qml" line="36"/>
-        <location filename="main.qml" line="37"/>
+        <location filename="Settings.qml" line="8"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="60"/>
+        <location filename="Settings.qml" line="45"/>
+        <source>用户名:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.qml" line="50"/>
+        <source>用户名/邮箱地址</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Settings.qml" line="53"/>
+        <source>验证码:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="17"/>
+        <source>Douban.FM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="21"/>
+        <location filename="main.qml" line="22"/>
+        <location filename="main.qml" line="331"/>
+        <source>Like</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="21"/>
+        <location filename="main.qml" line="22"/>
+        <source>UnLike</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="37"/>
+        <location filename="main.qml" line="38"/>
+        <location filename="main.qml" line="335"/>
+        <source>Ban</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="50"/>
+        <location filename="main.qml" line="51"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="78"/>
         <source>我的私人</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="60"/>
-        <location filename="main.qml" line="61"/>
+        <location filename="main.qml" line="78"/>
+        <location filename="main.qml" line="79"/>
         <source>私人电台</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="61"/>
+        <location filename="main.qml" line="79"/>
         <source>我的红星</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="63"/>
+        <location filename="main.qml" line="81"/>
         <source>华语</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="63"/>
-        <location filename="main.qml" line="64"/>
-        <location filename="main.qml" line="65"/>
-        <location filename="main.qml" line="66"/>
-        <location filename="main.qml" line="67"/>
-        <location filename="main.qml" line="68"/>
-        <location filename="main.qml" line="69"/>
-        <location filename="main.qml" line="70"/>
-        <location filename="main.qml" line="71"/>
-        <location filename="main.qml" line="72"/>
-        <location filename="main.qml" line="73"/>
-        <location filename="main.qml" line="74"/>
-        <location filename="main.qml" line="75"/>
-        <location filename="main.qml" line="76"/>
-        <location filename="main.qml" line="77"/>
-        <location filename="main.qml" line="78"/>
-        <location filename="main.qml" line="79"/>
-        <location filename="main.qml" line="80"/>
+        <location filename="main.qml" line="81"/>
+        <location filename="main.qml" line="82"/>
+        <location filename="main.qml" line="83"/>
+        <location filename="main.qml" line="84"/>
+        <location filename="main.qml" line="85"/>
+        <location filename="main.qml" line="86"/>
+        <location filename="main.qml" line="87"/>
+        <location filename="main.qml" line="88"/>
+        <location filename="main.qml" line="89"/>
+        <location filename="main.qml" line="90"/>
+        <location filename="main.qml" line="91"/>
+        <location filename="main.qml" line="92"/>
+        <location filename="main.qml" line="93"/>
+        <location filename="main.qml" line="94"/>
+        <location filename="main.qml" line="95"/>
+        <location filename="main.qml" line="96"/>
+        <location filename="main.qml" line="97"/>
+        <location filename="main.qml" line="98"/>
         <source>公共电台</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="64"/>
+        <location filename="main.qml" line="82"/>
         <source>粤语</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="65"/>
+        <location filename="main.qml" line="83"/>
         <source>欧美</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="66"/>
+        <location filename="main.qml" line="84"/>
         <source>法语</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="67"/>
+        <location filename="main.qml" line="85"/>
         <source>日语</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="68"/>
+        <location filename="main.qml" line="86"/>
         <source>韩语</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="69"/>
+        <location filename="main.qml" line="87"/>
         <source>70</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="70"/>
+        <location filename="main.qml" line="88"/>
         <source>80</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="71"/>
+        <location filename="main.qml" line="89"/>
         <source>90</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="72"/>
+        <location filename="main.qml" line="90"/>
         <source>摇滚</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="73"/>
+        <location filename="main.qml" line="91"/>
         <source>民谣</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="74"/>
+        <location filename="main.qml" line="92"/>
         <source>轻音乐</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="75"/>
+        <location filename="main.qml" line="93"/>
         <source>电影原声</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="76"/>
+        <location filename="main.qml" line="94"/>
         <source>爵士</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="77"/>
+        <location filename="main.qml" line="95"/>
         <source>电子</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="78"/>
+        <location filename="main.qml" line="96"/>
         <source>说唱</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="79"/>
+        <location filename="main.qml" line="97"/>
         <source>R&amp;B</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="80"/>
+        <location filename="main.qml" line="98"/>
         <source>女生</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="105"/>
+        <location filename="main.qml" line="178"/>
         <source>Release to refresh</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.qml" line="105"/>
+        <location filename="main.qml" line="178"/>
         <source>Pull to refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="238"/>
+        <source>UserName:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="243"/>
+        <source>UserName/E-mail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="245"/>
+        <source>Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="253"/>
+        <source>Captcha:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="276"/>
+        <source>Click to get captcha image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="295"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="315"/>
+        <source>NickName</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="319"/>
+        <source>DJ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="320"/>
+        <location filename="main.qml" line="324"/>
+        <source>yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="320"/>
+        <location filename="main.qml" line="324"/>
+        <source>no</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="323"/>
+        <source>PRO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="327"/>
+        <source>TotalTrack</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="328"/>
+        <location filename="main.qml" line="332"/>
+        <location filename="main.qml" line="336"/>
+        <source>Tracks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="339"/>
+        <source>Logout</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
